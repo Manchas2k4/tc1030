275d64069aaf19d9427ca2a5d63ee62923255585
Pedro Perez <pperezm@tec.mx>
2022-05-20 10:34:30 -0500

    actitivity for session2

---
+++ session_02/example2/rational.h
@@ -0,0 +1,10 @@
+// =========================================================
+// File: rational.h
+// Author:
+// Date:
+// Description:
+// =========================================================
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+#endif
